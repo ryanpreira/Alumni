98547d08875fcb55b6ee7c43077606dfd3bc7f10
Ryan Pereira <ryanpreiradd@gmail.com>
2024-08-06 19:59:31 -0300

    Segunda versão do backend (com validações adicionadas)

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from 'express';
 import { users, User } from '../models/userModel';
+import * as yup from 'yup';
+
+const bodyValidation: yup.Schema<User> = yup.object().shape({
+  id: yup.number().required().default(users.length + 1),
+  nome: yup.string().required().min(3),
+  email: yup.string().email().required(),
+  senha: yup.string().required().min(6),
+  acesso_tipo: yup.string().required(),
+  data_registro: yup.date().required().default(() => new Date())
+})
 
 export const getUsers = (req: Request, res: Response) => {
   res.json(users);
 };
 
 export const getUserById = (req: Request, res: Response) => {
+
   const user = users.find(u => u.id === parseInt(req.params.id));
   if (user) {
     res.json(user);
@@ -14,23 +25,69 @@ export const getUserById = (req: Request, res: Response) => {
   }
 };
 
-export const createUser = (req: Request, res: Response) => {
+export const createUser = async (req: Request, res: Response) => {
+
+  let validatedData: User | undefined = undefined;
+
+  try {
+    validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
+  } catch(error) {
+    const yupError = error as yup.ValidationError;
+    const validationErrors: Record<string, string> = {}
+
+    yupError.inner.forEach( error => {
+      if(error.path === undefined) return;
+      validationErrors[error.path] = error.message;
+    })
+
+    return res.status(404).json({
+      errors: {
+        default: validationErrors
+      }
+    });
+  }
+
   const newUser: User = {
     id: users.length + 1,
     ...req.body,
     data_registro: new Date().toISOString()
   };
+
   users.push(newUser);
   res.status(201).json(newUser);
 };
 
-export const updateUser = (req: Request, res: Response) => {
+export const updateUser = async (req: Request, res: Response) => {
+  
   const user = users.find(u => u.id === parseInt(req.params.id));
+
+  let validatedData: User | undefined = undefined;
+
+  try {
+    validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
+  } catch(error) {
+    const yupError = error as yup.ValidationError;
+    const validationErrors: Record<string, string> = {}
+
+    yupError.inner.forEach( error => {
+      if(error.path === undefined) return;
+      validationErrors[error.path] = error.message;
+    })
+
+    return res.status(404).json({
+      errors: {
+        default: validationErrors
+      }
+    });
+  }
+
   if (user) {
     Object.assign(user, req.body);
     res.json(user);
   } else {
-    res.status(404).json({ message: 'User not found' });
+    res.status(404).json({ 
+      message: 'User not found' 
+    });
   }
 };
 
@@ -40,6 +97,8 @@ export const deleteUser = (req: Request, res: Response) => {
     users.splice(userIndex, 1);
     res.status(204).send();
   } else {
-    res.status(404).json({ message: 'User not found' });
+    res.status(404).json({
+      message: 'User not found'
+    });
   }
 };
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -4,10 +4,10 @@ export interface User {
   email: string;
   senha: string;
   acesso_tipo: string;
-  data_registro: string;
+  data_registro: Date;
 }
 
 export const users: User[] = [
-  { id: 1, nome: 'Charlotte Emma Aitchison', email: 'charlixcx@gmail.com', senha: 'guess', acesso_tipo: 'admin', data_registro: new Date().toISOString() },
-  { id: 2, nome: 'James Thomas Smith', email: 'jamiexx@gmail.com', senha: 'inwaves', acesso_tipo: 'graduate_student', data_registro: new Date().toISOString() }
+  { id: 1, nome: 'Charlotte Emma Aitchison', email: 'charlixcx@gmail.com', senha: 'uguess', acesso_tipo: 'admin', data_registro: new Date() },
+  { id: 2, nome: 'James Thomas Smith', email: 'jamiexx@gmail.com', senha: 'inwaves', acesso_tipo: 'graduate_student', data_registro: new Date() }
 ];
